7b49138ee6bfd0ee9a5213d3c6461ef707553484
titansarus <amirm137878@gmail.com>
2025-05-14 18:07:49 -0400

    Added Amirmahdi's Part

---
+++ NPB-CPP/NPB-OMP/LU/npbparams.hpp
@@ -0,0 +1,27 @@
+/* CLASS = A */
+					/*
+					  c  This file is generated automatically by the setparams utility.
+					  c  It sets the number of processors and the class_npb of the NPB
+					  c  in this directory. Do not modify it by hand.
+					 */
+
+/* full problem size */
+#define	ISIZ1	64
+#define	ISIZ2	64
+#define	ISIZ3	64
+/* number of iterations and how often to print the norm */
+#define	ITMAX_DEFAULT	250
+#define	INORM_DEFAULT	250
+#define	DT_DEFAULT	2.0
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "05 May 2025"
+#define NPBVERSION "4.1"
+#define LIBVERSION "201511"
+#define COMPILERVERSION "13.3.0"
+#define CS1 "g++ -std=c++14"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common "
+#define CS5 "-O3 -fopenmp -mcmodel=medium"
+#define CS6 "-O3 -fopenmp -mcmodel=medium"
+#define CS7 "randdp"
---
+++ NPB-CPP/NPB-TBB/LU/npbparams.hpp
@@ -0,0 +1,27 @@
+/* CLASS = B */
+					/*
+					  c  This file is generated automatically by the setparams utility.
+					  c  It sets the number of processors and the class_npb of the NPB
+					  c  in this directory. Do not modify it by hand.
+					 */
+
+/* full problem size */
+#define	ISIZ1	102
+#define	ISIZ2	102
+#define	ISIZ3	102
+/* number of iterations and how often to print the norm */
+#define	ITMAX_DEFAULT	250
+#define	INORM_DEFAULT	250
+#define	DT_DEFAULT	2.0
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "04 May 2025"
+#define NPBVERSION "4.1"
+#define LIBVERSION "11103"
+#define COMPILERVERSION "13.3.0"
+#define CS1 "g++ -std=c++14"
+#define CS2 "$(CC)"
+#define CS3 "-lm -ltbb -lpthread -L/home/titansarus/Docu..."
+#define CS4 "-I../common  -I/home/titansarus/Documents/C..."
+#define CS5 "-O3 -mcmodel=medium"
+#define CS6 "-O3 -mcmodel=medium"
+#define CS7 "randdp"
