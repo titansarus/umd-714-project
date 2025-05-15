f299a4963e87a887a70307d199d256e22b87c956
Alireza Borhani <a.borhani95@gmail.com>
2025-05-15 01:35:08 +0000

    Added by Alireza

---
+++ NPB-CPP/NPB-OMP/BT/npbparams.hpp
@@ -0,0 +1,21 @@
+/* CLASS = A */
+					/*
+					  c  This file is generated automatically by the setparams utility.
+					  c  It sets the number of processors and the class_npb of the NPB
+					  c  in this directory. Do not modify it by hand.
+					 */
+#define	PROBLEM_SIZE	64
+#define	NITER_DEFAULT	200
+#define	DT_DEFAULT	0.0008
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "13 May 2025"
+#define NPBVERSION "4.1"
+#define LIBVERSION "201511"
+#define COMPILERVERSION "8.5.0"
+#define CS1 "g++ -std=c++14"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common "
+#define CS5 "-O3 -fopenmp -mcmodel=medium -g"
+#define CS6 "-O3 -fopenmp -mcmodel=medium"
+#define CS7 "randdp"
