33715914ecbc6cd7792e3641e7d8bfd31a7319e4
acoppens-umd <acoppens@terpmail.umd.edu>
2025-05-14 20:28:33 -0400

    Added SP benchmarks

---
+++ NPB-CPP/NPB-PSTL/SP/npbparams.hpp
@@ -0,0 +1,20 @@
+/* CLASS = A */
+					/*
+					  c  This file is generated automatically by the setparams utility.
+					  c  It sets the number of processors and the class_npb of the NPB
+					  c  in this directory. Do not modify it by hand.
+					 */
+#define	PROBLEM_SIZE	64
+#define	NITER_DEFAULT	400
+#define	DT_DEFAULT	0.0015
+#define	CONVERTDOUBLE	FALSE
+#define COMPILETIME "13 May 2025"
+#define NPBVERSION "4.1"
+#define COMPILERVERSION "11.3.0"
+#define CS1 "g++ -std=c++20 "
+#define CS2 "$(CC) "
+#define CS3 "-lm -ltbb -lpthread  -L$(INTELTBB_LIBDIR) -Wl,-rpath,$(INTELTBB_LIBDIR) "
+#define CS4 "-I../common  -I../common -I$(INTELTBB_INCDIR)"
+#define CS5 "-O3 "
+#define CS6 "-O3 "
+#define CS7 "randdp "
